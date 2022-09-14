fdda2ce8cdb20ac3123776a170d226cac226c538
Petya4000 <pt.bartosh54321@gmail.com>
2022-09-14 05:06:46 +0300

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -18,28 +18,29 @@ public class FileReader {
             int start = str.indexOf(':');
             int end = str.indexOf('\n');
             String name = str.substring(start + 2, end - 1);
-            str = str.substring(end + 1, str.length() - 1);
+            str = str.substring(end + 1);
 
             start = str.indexOf(':');
             end = str.indexOf('\n');
             String age = str.substring(start + 2, end - 1);
             Integer intAge = age.isEmpty() ? null : Integer.parseInt(age);
-            str = str.substring(end + 1, str.length() - 1);
+            str = str.substring(end + 1);
 
             start = str.indexOf(':');
             end = str.indexOf('\n');
             String email = str.substring(start + 2, end - 1);
-            str = str.substring(end + 1, str.length() - 1);
+            str = str.substring(end + 1);
 
 
             start = str.indexOf(':');
-            end = str.length();
-            String phone = str.substring(start + 2, end);
+            end = str.indexOf('\n');
+            String phone = str.substring(start + 2, end - 1);
             Long longPhone;
-            if (!phone.isEmpty())
-                longPhone = Long.parseLong(phone);
-            else
+            if (phone.isEmpty() || phone.equals(" "))
                 longPhone = 0L;
+            else
+                longPhone = Long.parseLong(phone);
+
             return new Profile(name, intAge, email, longPhone);
 
 
