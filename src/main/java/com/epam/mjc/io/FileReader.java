1aa936b8f82176b67ec3e693cbfaf0c2c75e68ab
Petya4000 <pt.bartosh54321@gmail.com>
2022-09-14 03:38:55 +0300

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -8,15 +8,12 @@ import java.io.IOException;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        try {
-            FileInputStream fileInputStream = new FileInputStream(file);
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
             int ch;
-            StringBuilder stringBuilder = new StringBuilder("");
+            StringBuilder stringBuilder = new StringBuilder();
             while ((ch = fileInputStream.read()) != -1) {
-                char chr = (char) ch;
                 stringBuilder.append((char) ch);
             }
-            fileInputStream.close();
             String str = stringBuilder.toString();
             int start = str.indexOf(':'), end = str.indexOf('\n');
             String name = str.substring(start + 2, end - 1);
@@ -33,12 +30,15 @@ public class FileReader {
             String email = str.substring(start + 2, end - 1);
             str = str.substring(end + 1, str.length() - 1);
 
-            phone:
 
             start = str.indexOf(':');
-            end = str.length() - 1;
-            String phone = str.substring(start + 1, end);
-            Long longPhone = Long.parseLong(age);
+            end = str.length();
+            String phone = str.substring(start + 2, end);
+            Long longPhone;
+            if (!phone.isEmpty())
+                longPhone = Long.parseLong(phone);
+            else
+                longPhone = 0L;
             return new Profile(name, intAge, email, longPhone);
 
 
