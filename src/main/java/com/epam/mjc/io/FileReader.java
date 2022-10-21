5624dabf64551a44e1d14d9e3f7ac67bec4cb45a
Petya4000 <pt.bartosh54321@gmail.com>
2022-10-22 00:44:32 +0300

    com1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,47 +1,58 @@
 package com.epam.mjc.io;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 
 public class FileReader {
+
+    public static void main(String[] args) {
+
+            String str = "Name: Anna\n" +
+                    "Age: 25\n" +
+                    "Email: anna@mailserver.com\n" +
+                    "Phone: 1234567890";
+            String [] els = str.split("(\\n[a-zA-Z]+:\\s)|(Name: )");
+
+        List<String> list = Arrays.stream(els).collect(Collectors.toList());
+        list.remove(0);
+        //list.stream().forEach(s -> System.out.println(s.length()));
+
+
+    }
 
     public Profile getDataFromFile(File file) {
         try (FileInputStream fileInputStream = new FileInputStream(file)) {
             int ch;
             StringBuilder stringBuilder = new StringBuilder();
-            while ((ch = fileInputStream.read()) != -1) {
+            while ((ch = fileInputStream.read()) != -1)
                 stringBuilder.append((char) ch);
-            }
-            String str = stringBuilder.toString();
-            int start = str.indexOf(':');
-            int end = str.indexOf('\n');
-            String name = str.substring(start + 2, end - 1);
-            str = str.substring(end + 1);
 
-            start = str.indexOf(':');
-            end = str.indexOf('\n');
-            String age = str.substring(start + 2, end - 1);
-            Integer intAge = age.isEmpty() ? null : Integer.parseInt(age);
-            str = str.substring(end + 1);
+            String [] els = stringBuilder.toString().split("(\\r\\n[a-zA-Z]+:\\s)|(Name: )|(\\r\\n)");
 
-            start = str.indexOf(':');
-            end = str.indexOf('\n');
-            String email = str.substring(start + 2, end - 1);
-            str = str.substring(end + 1);
+            List<String> list = Arrays.stream(els).collect(Collectors.toList());
+            list.remove(0);
 
-            start = str.indexOf(':');
-            end = str.indexOf('\n');
-            String phone = str.substring(start + 2, end - 1);
-            Long longPhone;
-            if (phone.isEmpty() || phone.equals(" "))
-                longPhone = 0L;
-            else
-                longPhone = Long.parseLong(phone);
 
-            return new Profile(name, intAge, email, longPhone);
+             String name = list.get(0), email = list.get(2);
+             int age;
+             long phone;
+             try {
+                 age = Integer.parseInt(list.get(1));
+                 phone = Long.parseLong(list.get(3));
+             } catch (NumberFormatException e){
+                 e.printStackTrace();
+                 return null;
+             }
 
+            return new Profile(name, age, email, phone);
 
         } catch (IOException ex) {
             ex.printStackTrace();
