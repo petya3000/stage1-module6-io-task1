f0d9044d087fdecc28b513a640960b2ee832bb5e
Petya4000 <pt.bartosh54321@gmail.com>
2022-10-22 10:52:21 +0300

    last com

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,38 +1,24 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
-        try (FileInputStream fileInputStream = new FileInputStream(file)) {
-            int ch;
-            StringBuilder stringBuilder = new StringBuilder();
-            while ((ch = fileInputStream.read()) != -1)
-                stringBuilder.append((char) ch);
-
-            String[] els = stringBuilder.toString().split("(\\r\\n[a-zA-Z]+:\\s)|(Name: )|(\\r\\n)");
-
-            List<String> list = Arrays.stream(els).collect(Collectors.toList());
-            list.remove(0);
-
-
-            String name = list.get(0);
-            String email = list.get(2);
-            int age;
-            long phone;
-            age = Integer.parseInt(list.get(1));
-            phone = Long.parseLong(list.get(3));
+        String name;
+        int age;
+        String email;
+        long phone;
+        try (BufferedReader inputStream = new BufferedReader(new java.io.FileReader(file))) {
+            name = inputStream.readLine().substring(6);
+            age = Integer.parseInt(inputStream.readLine().substring(5));
+            email = inputStream.readLine().substring(7);
+            phone = Long.parseLong(inputStream.readLine().substring(7));
             return new Profile(name, age, email, phone);
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
             return null;
         }
     }
